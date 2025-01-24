a52b79f9d12bc774f08296d84df3efcb4566ea69
Victor Carrillo <gatitto079@gmail.com>
2025-01-23 21:35:40 -0600

    build(inicia el proyecto): :tada: primer push y commit
    primer push y commit del proyecto

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('GymRat API')
+    .setDescription('API de GymRat para desarrollo')
+    .setVersion('0.0.1')
+    .addTag('ejercicios')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
+  await app.listen(process.env.PORT ?? 3000);
+}
+void bootstrap();
