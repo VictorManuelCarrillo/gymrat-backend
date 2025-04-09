6bb3081d8f722ed22b0b7365ba0c3ede86282523
Baanggs <manuelgonzalez211104@gmail.com>
2025-04-09 11:34:40 -0600

    feat(rutina): :sparkles: se agrega controller, servce, module
    se agregag el controlador para rutinas , module,service

--- src/rutina/esquemas/rutinas.eschema.ts
+++
@@ -1,7 +0,0 @@
-import { Schema } from 'mongoose';
-
-export const RutinaSchema = new Schema({
-  name: { type: ' string', required: false },
-  description: 'string',
-  imageUrl: { type: 'string', required: false },
-});
